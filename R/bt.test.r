a99ee42c61eade2ea024615ec7f0b1bc8b38f398
systematicinvestor <thesystematicinvestor@gmail.com>
2012-05-07 23:59:12 -0400

    ok
    Signed-off-by: systematicinvestor <thesystematicinvestor@gmail.com>

--- R/bt.test.r
+++ R/bt.test.r
@@ -3033,6 +3033,82 @@ dev.off()
 }	
 
 
+###############################################################################
+# Rolling Correlation
+# http://www.activetradermag.com/index.php/c/Trading_Strategies/d/Trading_correlation
+###############################################################################
+bt.rolling.cor.test <- function() 
+{
+	#*****************************************************************
+	# Load historical data
+	#****************************************************************** 
+	load.packages('quantmod')	
+	tickers = sp500.components()$tickers
+	
+	data <- new.env()
+	getSymbols(tickers, src = 'yahoo', from = '1970-01-01', env = data, auto.assign = T)
+		for(i in ls(data)) data[[i]] = adjustOHLC(data[[i]], use.Adjusted=T)		
+	bt.prep(data, align='keep.all', dates='1970::')
+
+	spy = getSymbols('SPY', src = 'yahoo', from = '1970-01-01', auto.assign = F)
+		ret.spy = coredata( Cl(spy) / mlag(Cl(spy))-1 )
+	
+	#*****************************************************************
+	# Code Logic
+	#****************************************************************** 
+	prices = data$prices['1993:01:29::']  
+		nperiods = nrow(prices)
+			
+	ret = prices / mlag(prices) - 1
+		ret = coredata(ret)
+		
+	# require at least 100 stocks with prices
+	index = which((count(t(prices)) > 100 ))
+		index = index[-c(1:252)]
+		
+	# average correlation among S&P 500 components
+	avg.cor = NA * prices[,1]
+	
+	# average correlation between the S&P 500 index (SPX) and its component stocks
+	avg.cor.spy = NA * prices[,1]
+	
+	for(i in index) {
+		hist = ret[ (i- 252 +1):i, ]
+		hist = hist[ , count(hist)==252, drop=F]
+			nleft = ncol(hist)
+		
+		correlation = cor(hist, use='complete.obs',method='pearson')
+		avg.cor[i,] = (sum(correlation) - nleft) / (nleft*(nleft-1))
+		
+		avg.cor.spy[i,] = sum(cor(ret.spy[ (i- 252 +1):i, ], hist, use='complete.obs',method='pearson')) / nleft
+		
+		if( i %% 100 == 0) cat(i, 'out of', nperiods, '\n')
+	}
+		
+	#*****************************************************************
+	# Create Report
+	#****************************************************************** 				
+png(filename = 'plot.sp500.cor.png', width = 600, height = 500, units = 'px', pointsize = 12, bg = 'white')										
+
+	
+ 	sma50 = SMA(Cl(spy), 50)
+ 	sma200 = SMA(Cl(spy), 200)
+ 	
+ 	cols = col.add.alpha(spl('green,red'),50)
+	plota.control$col.x.highlight = iif(sma50 > sma200, cols[1], cols[2])
+	highlight = sma50 > sma200 | sma50 < sma200
+			
+	plota(avg.cor, type='l', ylim=range(avg.cor, avg.cor.spy, na.rm=T), x.highlight = highlight,
+			main='Average 252 day Pairwise Correlation for stocks in SP500')
+		plota.lines(avg.cor.spy, type='l', col='blue')
+		plota.legend('Pairwise Correlation,Correlation with SPY,SPY 50-day SMA > 200-day SMA,SPY 50-day SMA < 200-day SMA', 
+		c('black,blue',cols))
+		
+dev.off()	
+		
+}
+
+
 
 
 
