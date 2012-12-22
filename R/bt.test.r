59b88872f23a59c8df8bfccf26d3e718d58f84b7
systematicinvestor <thesystematicinvestor@gmail.com>
2012-12-22 13:14:04 -0500

    ok

--- R/bt.test.r
+++ R/bt.test.r
@@ -6802,7 +6802,7 @@ bt.financial.turbulence.test <- function()
 	#*****************************************************************
 	# Plot 30 day average of the Financial Turbulence for G10 Currencies
 	#****************************************************************** 	
-png(filename = 'plot1.png', width = 600, height = 500, units = 'px', pointsize = 12, bg = 'white')										    			
+png(filename = 'plot1.png', width = 600, height = 500, units = 'px', pointsize = 12, bg = 'white')
 	plota(EMA( turbulence, 30), type='l', 
 		main='30 day average of the Financial Turbulence for G10 Currencies')
 dev.off()	
@@ -6810,4 +6810,103 @@ dev.off()
 	
 }	
 	
-					
+	
+
+###############################################################################
+# Principal component analysis (PCA)
+###############################################################################		
+bt.pca.test <- function()
+{			
+	#*****************************************************************
+	# Find Sectors for each company in DOW 30
+	#****************************************************************** 
+	tickers = spl('XLY,XLP,XLE,XLF,XLV,XLI,XLB,XLK,XLU')
+	tickers.desc = spl('ConsumerCyclicals,ConsumerStaples,Energy,Financials,HealthCare,Industrials,Materials,Technology,Utilities')
+	
+	sector.map = c()
+	for(i in 1:len(tickers)) {
+		sector.map = rbind(sector.map, 
+				cbind(sector.spdr.components(tickers[i]), tickers.desc[i])
+			)
+	}
+	colnames(sector.map) = spl('ticker,sector')
+
+	#*****************************************************************
+	# Load historical data
+	#****************************************************************** 
+	load.packages('quantmod')	
+	tickers = dow.jones.components()
+	
+	sectors = factor(sector.map[ match(tickers, sector.map[,'ticker']), 'sector'])
+		names(sectors) = tickers
+	
+	data <- new.env()
+	getSymbols(tickers, src = 'yahoo', from = '2000-01-01', env = data, auto.assign = T)
+		for(i in ls(data)) data[[i]] = adjustOHLC(data[[i]], use.Adjusted=T)	
+	
+	bt.prep(data, align='keep.all', dates='2012')
+	
+	# re-order sectors, because bt.prep can change the order of tickers
+	sectors = sectors[data$symbolnames]
+	
+	# save data for later examples
+	save(data, tickers, sectors, file='bt.pca.test.Rdata')
+	#load(file='bt.pca.test.Rdata')
+
+	#*****************************************************************
+	# Principal component analysis (PCA), for interesting discussion
+	# http://machine-master.blogspot.ca/2012/08/pca-or-polluting-your-clever-analysis.html
+	#****************************************************************** 
+	prices = data$prices	
+	ret = prices / mlag(prices) - 1
+	
+	p = princomp(na.omit(ret))
+	
+	loadings = p$loadings[]
+	p.variance.explained = p$sdev^2 / sum(p$sdev^2)
+
+	# plot percentage of variance explained for each principal component	
+png(filename = 'plot1.png', width = 600, height = 500, units = 'px', pointsize = 12, bg = 'white')	
+	barplot(100*p.variance.explained, las=2, xlab='', ylab='% Variance Explained')
+dev.off()
+	
+	#*****************************************************************
+	# 2-D Plot
+	#****************************************************************** 		
+	x = loadings[,1]
+	y = loadings[,2]
+	z = loadings[,3]
+	cols = as.double(sectors)
+	
+	# plot all companies loadings on the first and second principal components and highlight points according to the sector they belong
+png(filename = 'plot2.png', width = 600, height = 500, units = 'px', pointsize = 12, bg = 'white')		
+	plot(x, y, type='p', pch=20, col=cols, xlab='Comp.1', ylab='Comp.2')
+	text(x, y, data$symbolnames, col=cols, cex=.8, pos=4)
+	
+	legend('topright', cex=.8,  legend = levels(sectors), fill = 1:nlevels(sectors), merge = F, bty = 'n') 
+dev.off()
+
+	#*****************************************************************
+	# 3-D Plot, for good examples of 3D plots
+	# http://statmethods.wordpress.com/2012/01/30/getting-fancy-with-3-d-scatterplots/
+	#****************************************************************** 				
+	load.packages('scatterplot3d') 
+	
+	# plot all companies loadings on the first, second, and third principal components and highlight points according to the sector they belong
+png(filename = 'plot3.png', width = 600, height = 500, units = 'px', pointsize = 12, bg = 'white')			
+	s3d = scatterplot3d(x, y, z, xlab='Comp.1', ylab='Comp.2', zlab='Comp.3', color=cols, pch = 20)
+		
+    s3d.coords = s3d$xyz.convert(x, y, z)
+    text(s3d.coords$x, s3d.coords$y, labels=data$symbolnames, col=cols, cex=.8, pos=4)
+		
+    legend('topleft', cex=.8,  legend = levels(sectors), fill = 1:nlevels(sectors), merge = F, bty = 'n') 
+dev.off()
+
+	#*****************************************************************
+	# Next steps          
+	#*****************************************************************
+    # - demonstrate clustering based on the selected Principal components
+    # - using PCA for spread trading 
+    # http://matlab-trading.blogspot.ca/2012/12/using-pca-for-spread-trading.html
+}    
+				
